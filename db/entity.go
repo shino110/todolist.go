f9b943aa5ff1c384e6ac4be1d8a9e55478a17972
Shino Ito <ito.s.cd@m.titech.ac.jp>
2023-10-21 15:37:10 +0900

    Initial commit

---
+++ db/entity.go
@@ -0,0 +1,14 @@
+package db
+
+// schema.go provides data models in DB
+import (
+	"time"
+)
+
+// Task corresponds to a row in `tasks` table
+type Task struct {
+	ID        uint64    `db:"id"`
+	Title     string    `db:"title"`
+	CreatedAt time.Time `db:"created_at"`
+	IsDone    bool      `db:"is_done"`
+}
---
+++ service/task.go
@@ -0,0 +1,58 @@
+package service
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	database "todolist.go/db"
+)
+
+// TaskList renders list of tasks in DB
+func TaskList(ctx *gin.Context) {
+	// Get DB connection
+	db, err := database.GetConnection()
+	if err != nil {
+		Error(http.StatusInternalServerError, err.Error())(ctx)
+		return
+	}
+
+	// Get tasks in DB
+	var tasks []database.Task
+	err = db.Select(&tasks, "SELECT * FROM tasks") // Use DB#Select for multiple entries
+	if err != nil {
+		Error(http.StatusInternalServerError, err.Error())(ctx)
+		return
+	}
+
+	// Render tasks
+	ctx.HTML(http.StatusOK, "task_list.html", gin.H{"Title": "Task list", "Tasks": tasks})
+}
+
+// ShowTask renders a task with given ID
+func ShowTask(ctx *gin.Context) {
+	// Get DB connection
+	db, err := database.GetConnection()
+	if err != nil {
+		Error(http.StatusInternalServerError, err.Error())(ctx)
+		return
+	}
+
+	// parse ID given as a parameter
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		Error(http.StatusBadRequest, err.Error())(ctx)
+		return
+	}
+
+	// Get a task with given ID
+	var task database.Task
+	err = db.Get(&task, "SELECT * FROM tasks WHERE id=?", id) // Use DB#Get for one entry
+	if err != nil {
+		Error(http.StatusBadRequest, err.Error())(ctx)
+		return
+	}
+
+	// Render task
+	ctx.String(http.StatusOK, task.Title)  // Modify it!!
+}
