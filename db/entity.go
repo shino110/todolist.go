88d73ba714542d97c79c0fb41e0591efcaadce70
Shino Ito <ito.s.cd@m.titech.ac.jp>
2023-10-26 20:51:35 +0900

    231026 fin 6-3

--- db/entity.go
+++ db/entity.go
@@ -2,14 +2,15 @@ package db
 
 // schema.go provides data models in DB
 import (
+	"database/sql"
 	"time"
 )
 
 // Task corresponds to a row in `tasks` table
 type Task struct {
-	ID        uint64    `db:"id"`
-	Title     string    `db:"title"`
-	CreatedAt time.Time `db:"created_at"`
-	Memo      string    `db:"memo"`
-	IsDone    bool      `db:"is_done"`
+	ID        uint64         `db:"id"`
+	Title     string         `db:"title"`
+	CreatedAt time.Time      `db:"created_at"`
+	Memo      sql.NullString `db:"memo"`
+	IsDone    bool           `db:"is_done"`
 }
--- docker/db/sql/02_initial_data.sql
+++ docker/db/sql/02_initial_data.sql
@@ -1,5 +1,5 @@
-INSERT INTO `tasks` (`title`, `memo`) VALUES ("sample-task-01", " ");
-INSERT INTO `tasks` (`title`, `memo`) VALUES ("sample-task-02", " ");
-INSERT INTO `tasks` (`title`, `memo`) VALUES ("sample-task-03", " ");
+INSERT INTO `tasks` (`title`) VALUES ("sample-task-01");
+INSERT INTO `tasks` (`title`) VALUES ("sample-task-02");
+INSERT INTO `tasks` (`title`) VALUES ("sample-task-03");
 INSERT INTO `tasks` (`title`, `memo`) VALUES ("sample-task-04", "memomemo");
-INSERT INTO `tasks` (`title`, `is_done`, `memo`) VALUES ("sample-task-05", true, " ");
+INSERT INTO `tasks` (`title`, `is_done`) VALUES ("sample-task-05", true);
